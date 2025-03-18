b6b9237acabfa3049ce4f25ab1c56d2a4918790a
neha-lawi <neha.mer@mobavenue.com>
2025-03-18 16:27:46 +0530

    changes

--- application/config/constants.php
+++ application/config/constants.php
@@ -85,4 +85,5 @@ defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automat
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
 #custom constant
-define('URL','http://localhost/awsproject');
+// define('URL','http://localhost/awsproject');
+define('URL','http://3.83.11.187/');
